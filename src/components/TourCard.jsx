00bff0912af0c77144889514ebaa970252c491e3
cstone45 <cstone45@usf.edu>
2025-04-27 16:43:35 -0400

    Added a "Show More" and "Show Less" Button and a corresponding update to button styles

--- src/components/TourCard.jsx
+++ src/components/TourCard.jsx
@@ -1,14 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function TourCard({ tour, onRemove }) {
   const { id, name, info, image, price } = tour;
+  const [showMore, setShowMore] = useState(false);
 
   return (
     <div className="tour-card">
       <img src={image} alt={name} className="tour-image" />
       <div className="tour-details">
         <h3>{name}</h3>
-        <p>{info}</p>
+        <p>
+          {showMore ? info : `${info.substring(0, 100)}...`}
+          <button
+            onClick={() => setShowMore(!showMore)}
+            className="toggle-description-btn"
+          >
+            {showMore ? 'Show Less' : 'Show More'}
+          </button>
+        </p>
         <h4>Price: ${price}</h4>
         <button onClick={() => onRemove(id)} className="not-interested-btn">
           Not Interested
